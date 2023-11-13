ec8e68e88957055689eaa219cc9af6a77d2bc02a
Vedikamevawala <143768738+Vedikamevawala@users.noreply.github.com>
2023-11-12 18:58:15 -0600

    changes

---
+++ script.js
@@ -0,0 +1,25 @@
+function init(){
+ function myEventFunction (){
+  alert('Hello' );
+ }
+ var d4 = document.getElementById('div4');
+ d4.addEventListener('click', myEventFunction);
+ d4.addEventListener('click', function){
+  console.log('I was Clicked');
+  d4.style.backgroundColor = '#c00';
+  d4.innerHTML = '<strong> Goodbye!</strong>';
+ }
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+}
+window.addEventListener('load', init);}
